3939af7a387db42cb5ae13000909966974ffeb8e
Andrei <andrei.florea003@gmail.com>
2024-05-20 11:03:42 -0700

    Added horizontal lines to schedule visualizer

--- lib/widgets/course_visualizer.dart
+++ lib/widgets/course_visualizer.dart
@@ -16,54 +16,62 @@ class CourseVisualizer extends StatelessWidget {
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
       return Scaffold(
-          body: (Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          TimeColumn(),
-          VerticalDivider(
-            color: Colors.black,
-          ),
-          ScheduleColumn(
-            name: 'Monday',
-            courses: courses,
-          ),
-          VerticalDivider(
-            color: Colors.black,
-          ),
-          ScheduleColumn(
-            name: 'Tuesday',
-            courses: courses,
-          ),
-          VerticalDivider(
-            color: Colors.black,
-          ),
-          ScheduleColumn(
-            name: 'Wednesday',
-            courses: courses,
-          ),
-          VerticalDivider(
-            color: Colors.black,
-          ),
-          ScheduleColumn(
-            name: 'Thursday',
-            courses: courses,
-          ),
-          VerticalDivider(
-            color: Colors.black,
-          ),
-          ScheduleColumn(
-            name: 'Friday',
-            courses: courses,
-          ),
-          VerticalDivider(
-            color: Colors.black,
-          ),
-          ScheduleColumn(
-            name: 'Saturday',
-            courses: courses,
-          ),
-        ],
-      )));
+          body: (Stack(children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            TimeColumn(),
+            Spacer(),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            VerticalDivider(
+              color: Colors.black,
+            ),
+            ScheduleColumn(
+              name: 'Monday',
+              courses: courses,
+            ),
+            VerticalDivider(
+              color: Colors.black,
+            ),
+            ScheduleColumn(
+              name: 'Tuesday',
+              courses: courses,
+            ),
+            VerticalDivider(
+              color: Colors.black,
+            ),
+            ScheduleColumn(
+              name: 'Wednesday',
+              courses: courses,
+            ),
+            VerticalDivider(
+              color: Colors.black,
+            ),
+            ScheduleColumn(
+              name: 'Thursday',
+              courses: courses,
+            ),
+            VerticalDivider(
+              color: Colors.black,
+            ),
+            ScheduleColumn(
+              name: 'Friday',
+              courses: courses,
+            ),
+            VerticalDivider(
+              color: Colors.black,
+            ),
+            ScheduleColumn(
+              name: 'Saturday',
+              courses: courses,
+            ),
+          ],
+        )
+      ])));
     });
   }
 }
@@ -74,6 +82,8 @@ class TimeColumn extends StatelessWidget {
   });
 
   final String name = "Time";
+  final double width = 1400;
+  final double height = 0.5;
 
   @override
   Widget build(BuildContext context) {
@@ -83,52 +93,101 @@ class TimeColumn extends StatelessWidget {
           child: Spacer(),
         ),
         Expanded(
-          child: Text("7:00 AM"),
+          child: Row(children: [
+            Text("7:00 AM"),
+            Container(width: width, height: height, color: Colors.black),
+          ]),
         ),
         Expanded(
-          child: Text("8:00 AM"),
+            child: Row(
+          children: [
+            Text("8:00 AM"),
+            Container(width: width, height: height, color: Colors.black),
+          ],
+        )),
+        Expanded(
+          child: Row(children: [
+            Text("9:00 AM"),
+            Container(width: width, height: height, color: Colors.black),
+          ]),
         ),
         Expanded(
-          child: Text("9:00 AM"),
+          child: Row(children: [
+            Text("10:00 AM"),
+            Container(width: width, height: height, color: Colors.black),
+          ]),
         ),
         Expanded(
-          child: Text("10:00 AM"),
+          child: Row(children: [
+            Text("11:00 AM"),
+            Container(width: width, height: height, color: Colors.black),
+          ]),
         ),
         Expanded(
-          child: Text("11:00 AM"),
+          child: Row(children: [
+            Text("12:00 PM"),
+            Container(width: width, height: height, color: Colors.black),
+          ]),
         ),
         Expanded(
-          child: Text("12:00 PM"),
+          child: Row(children: [
+            Text("1:00 PM"),
+            Container(width: width, height: height, color: Colors.black),
+          ]),
         ),
         Expanded(
-          child: Text("1:00 PM"),
+          child: Row(children: [
+            Text("2:00 PM"),
+            Container(width: width, height: height, color: Colors.black),
+          ]),
         ),
         Expanded(
-          child: Text("2:00 PM"),
+          child: Row(children: [
+            Text("3:00 PM"),
+            Container(width: width, height: height, color: Colors.black),
+          ]),
         ),
         Expanded(
-          child: Text("3:00 PM"),
+          child: Row(children: [
+            Text("4:00 PM"),
+            Container(width: width, height: height, color: Colors.black),
+          ]),
         ),
         Expanded(
-          child: Text("4:00 PM"),
+          child: Row(children: [
+            Text("5:00 PM"),
+            Container(width: width, height: height, color: Colors.black),
+          ]),
         ),
         Expanded(
-          child: Text("5:00 PM"),
+          child: Row(children: [
+            Text("6:00 PM"),
+            Container(width: width, height: height, color: Colors.black),
+          ]),
         ),
         Expanded(
-          child: Text("6:00 PM"),
+          child: Row(children: [
+            Text("7:00 PM"),
+            Container(width: width, height: height, color: Colors.black),
+          ]),
         ),
         Expanded(
-          child: Text("7:00 PM"),
+          child: Row(children: [
+            Text("8:00 PM"),
+            Container(width: width, height: height, color: Colors.black),
+          ]),
         ),
         Expanded(
-          child: Text("8:00 PM"),
+          child: Row(children: [
+            Text("9:00 PM"),
+            Container(width: width, height: height, color: Colors.black),
+          ]),
         ),
         Expanded(
-          child: Text("9:00 PM"),
-        ),
-        Expanded(
-          child: Text("10:00 PM"),
+          child: Row(children: [
+            Text("10:00 PM"),
+            Container(width: width, height: height, color: Colors.black),
+          ]),
         ),
         Spacer(),
         Text(name),
